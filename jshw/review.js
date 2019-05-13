1e293d4fedf5582ee9789bf678f48a9991fef7c6
koyouavang94 <50379999+koyouavang94@users.noreply.github.com>
2019-05-13 08:49:28 -0500

    Add files via upload

---
+++ jshw/review.js
@@ -0,0 +1,54 @@
+var homeboys = 'chung, pete, eric, orion';
+var went_to_school_with = false;
+var how_many = 4;
+console.log(homeboys);
+console.log(went_to_school_with);
+console.log(how_many);
+
+var equation1 = 5 + 5;
+var equation2 = equation1 * 1;
+var equation3 = 12-5; 
+
+var home_items = [
+    'spoons',
+    'toothbrush',
+    'towels',
+    'screw driver'
+];
+
+console.log(home_items);
+home_items.reverse();
+console.log(home_items);
+
+var math = [
+    equation1,
+    equation2,
+    equation3
+];
+
+console.log(home_items);
+console.log(math);
+
+var siblings_age = [
+    40,
+    38,
+    22,
+    21,
+    18
+];
+
+siblings_age.push(32);
+
+console.log(siblings_age);
+console.log(siblings_age.splice(1,5));
+
+console.log(typeof homeboys);
+console.log(typeof home_items);
+
+var age = '60th';
+var age_sentence = `Happy ${age} birthday mom! I love you!`;
+console.log(age_sentence);
+
+
+
+
